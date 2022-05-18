189fabc4f09b6abc8b4f1de67e375684b90af536
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 <artrok@github.com>
2022-05-18 17:36:45 -0300

    ✅ test: main frame created

--- frontend/src/tests/MainPage.test.js
+++ frontend/src/tests/MainPage.test.js
@@ -83,6 +83,8 @@ describe('Testing Main Page', () => {
   describe("Task Panel", () => {
     beforeAll(() => {
       axios.get.mockResolvedValue(axiosGetMock);
+      axios.put.mockResolvedValue(axiosGetMock[1]);
+      axios.delete.mockResolvedValue({});
     });
 
     test("Should have Titles, Dates, Timestamps, Creation date, Details and Status", () => {
@@ -91,17 +93,63 @@ describe('Testing Main Page', () => {
       const tasksTitles = screen.getAllByTestId('tasksTitles');
       const tasksDates = screen.getAllByTestId('tasksDates');
       const tasksTimestamps = screen.getAllByTestId('tasksTimestamps');
-      const tasksCreationDate = screen.getAllByTestId('tasksCreationDate');
+      const tasksCreationDates = screen.getAllByTestId('tasksCreationDates');
       const tasksDetails = screen.getAllByTestId('tasksDetails');
       const tasksStatus = screen.getAllByTestId('tasksStatus');
       
       expect(tasksTitles).toBeInTheDocument();
       expect(tasksDates).toBeInTheDocument();
       expect(tasksTimestamps).toBeInTheDocument();
-      expect(tasksCreationDate).toBeInTheDocument();
+      expect(tasksCreationDates).toBeInTheDocument();
       expect(tasksDetails).toBeInTheDocument();
       expect(tasksStatus).toBeInTheDocument();
       
+    });
+
+    test("Should have edit task option", async () => {
+      renderWithRouter(<App />);
+
+      const editTaskButton = await screen.findByRole('button', {name: /editTask1/});
+      expect(editTaskButton).toBeInTheDocument();
+      
+      userEvent.click(editTaskButton);
+      
+      const taskTitle = await screen.findByRole('input', {name: /taskTitle1/});
+      const taskDate = await screen.findByRole('input', {name: /taskDate1/});
+      const taskTimestamp = await screen.findByRole('input', {name: /taskTimestamp1/});
+      const taskCreationDate = await screen.findByRole('input', {name: /taskCreationDate1/});
+      const taskDetail = await screen.findByRole('input', {name: /taskDetail1/});
+      const taskStatus = await screen.findByRole('input', {name: /taskStatus1/});
+      
+      expect(taskTitle).toBeInTheDocument();
+      expect(taskDate).toBeInTheDocument();
+      expect(taskTimestamp).toBeInTheDocument();
+      expect(taskCreationDate).toBeInTheDocument();
+      expect(taskDetail).toBeInTheDocument();
+      expect(taskStatus).toBeInTheDocument();
+    });
+
+    test("Should have delete task option", async () => {
+      renderWithRouter(<App />);
+
+      const deleteTaskButton = await screen.findByRole('button', {name: /deleteTask1/});
+      expect(deleteTaskButton).toBeInTheDocument();
+
+      userEvent.click(deleteTaskButton);
+
+      const taskTitle = await screen.findByTestId("taskTitle1");
+      const taskDate = await screen.findByTestId("taskDate1");
+      const taskTimestamp = await screen.findByTestId("taskTimestamp1");
+      const taskCreationDate = await screen.findByTestId("taskCreationDate1");
+      const taskDetail = await screen.findByTestId("taskDetail1");
+      const taskStatus = await screen.findByTestId("taskStatus1");
+      
+      expect(taskTitle).not.toBeInTheDocument();
+      expect(taskDate).not.toBeInTheDocument();
+      expect(taskTimestamp).not.toBeInTheDocument();
+      expect(taskCreationDate).not.toBeInTheDocument();
+      expect(taskDetail).not.toBeInTheDocument();
+      expect(taskStatus).not.toBeInTheDocument();
     });
   });
 })
