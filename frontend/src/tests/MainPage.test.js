895ffecf0d7b1ea016af6e5c0d6fb2ca039d0db3
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 <artrok@github.com>
2022-05-18 16:52:30 -0300

    ✅ test: created a mock and improved

--- frontend/src/tests/MainPage.test.js
+++ frontend/src/tests/MainPage.test.js
@@ -3,6 +3,28 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './RenderWithRouter';
 import App from '../App';
+import axios from 'axios';
+
+const axiosGetMock = [{
+  id: 1,
+  title: "My first activity",
+  date: "06/06/2022",
+  time: "12:00",
+  details: "A very important activity that will change my life",
+  status: "in progress",
+  creationAt: "06/06/2022",
+},
+{
+  id: 2,
+  title: "My second activity",
+  date: "07/06/2022",
+  time: "13:00",
+  details: "An important activity that will change my life too",
+  status: "pending",
+  creationAt: "06/06/2022",
+}];
+
+jest.mock('axios');
 
 describe('Testing Main Page', () => {
   describe('Task Menu', () => {
@@ -59,24 +81,12 @@ describe('Testing Main Page', () => {
     });
   });
   describe("Task Panel", () => {
+    beforeAll(() => {
+      axios.get.mockResolvedValue(axiosGetMock);
+    });
+
     test("Should have Titles, Dates, Timestamps, Creation date, Details and Status", () => {
       renderWithRouter(<App />);
-      const addTaskButton = screen.getByRole('button', { name: /add new task/i });
-      userEvent.click(addTaskButton);
-
-      const dateField = screen.getByRole('input', { name: /date/i });
-      const timeField = screen.getByRole('input', { name: /time/i });
-      const titleField = screen.getByRole('input', { name: /title/i });
-      const detailsField = screen.getByRole('input', { name: /details/i });
-      const progressField = screen.getByRole('input', { name: /progress/i });
-      userEvent.type(dateField, "06/06/2022");
-      userEvent.type(timeField, "12:00");
-      userEvent.type(titleField, "My first activity");
-      userEvent.type(detailsField, "A very important activity that will change my life");
-      userEvent.type(progressField, "pending");
-
-      const createTaskButton = screen.getByRole('button', { name: /create task/i });
-      userEvent.click(createTaskButton);
 
       const tasksTitles = screen.getAllByTestId('tasksTitles');
       const tasksDates = screen.getAllByTestId('tasksDates');
