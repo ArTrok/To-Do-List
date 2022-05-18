8bdc6dd142302453e76f673625f1c4271d061588
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 <artrok@github.com>
2022-05-18 15:42:30 -0300

    ✅test: changed name and added a few more scenarios

--- frontend/tests/MainPage.test.js
+++ frontend/tests/MainPage.test.js
@@ -6,19 +6,20 @@ import App from '../App';
 
 describe('Testing Main Page', () => {
   describe('Task Menu', () => {
-    test('Should have menu', () => {
-
+    test('Should have Menu', () => {
+      renderWithRouter(<App />);
+      const menu = screen.getByTestId('taskMenu');
+      expect(menu).toBeInTheDocument();
     });
     test('Should have menu option to add a new task', () => {
       renderWithRouter(<App />);
-      const menu = screen.getAllByTestId('taskMenu');
-      expect(menu).toBeInTheDocument();
+      const addTaskButton = screen.getByRole('button', { name: /add new task/i });
+      expect(addTaskButton).toBeInTheDocument();
     });
 
     test('Should have fileds "Date, Time, Title, Details and Progress" to add a new task', () => {
       renderWithRouter(<App />);
       const addTaskButton = screen.getByRole('button', { name: /add new task/i });
-      expect(addTaskButton).toBeInTheDocument();
       userEvent.click(addTaskButton);
       const dateField = screen.getByRole('input', { name: /date/i });
       const timeField = screen.getByRole('input', { name: /time/i });
@@ -35,9 +36,8 @@ describe('Testing Main Page', () => {
     test('Should fill all fileds and hit create to add a new task', async () => {
       renderWithRouter(<App />);
       const addTaskButton = screen.getByRole('button', { name: /add new task/i });
-      const createTaskButton = screen.getByRole('button', { name: /create task/i });
-      expect(addTaskButton).toBeInTheDocument();
       userEvent.click(addTaskButton);
+
       const dateField = screen.getByRole('input', { name: /date/i });
       const timeField = screen.getByRole('input', { name: /time/i });
       const titleField = screen.getByRole('input', { name: /title/i });
@@ -48,17 +48,22 @@ describe('Testing Main Page', () => {
       userEvent.type(titleField, "My first activity");
       userEvent.type(detailsField, "A very important activity that will change my life");
       userEvent.type(progressField, "pending");
+
+      const createTaskButton = screen.getByRole('button', { name: /create task/i });
+      expect(createTaskButton).toBeInTheDocument();
+
       userEvent.click(createTaskButton);
+      
       const taskCreatedText = await screen.findByText(/task created/i);
       expect(taskCreatedText).toBeInTheDocument();
     });
   });
   describe("Task Panel", () => {
-    beforeAll(() => {
+    test("Should have Titles, Dates, Timestamps, Creation date, Details and Status", () => {
       renderWithRouter(<App />);
       const addTaskButton = screen.getByRole('button', { name: /add new task/i });
-      const createTaskButton = screen.getByRole('button', { name: /create task/i });
       userEvent.click(addTaskButton);
+
       const dateField = screen.getByRole('input', { name: /date/i });
       const timeField = screen.getByRole('input', { name: /time/i });
       const titleField = screen.getByRole('input', { name: /title/i });
@@ -69,7 +74,24 @@ describe('Testing Main Page', () => {
       userEvent.type(titleField, "My first activity");
       userEvent.type(detailsField, "A very important activity that will change my life");
       userEvent.type(progressField, "pending");
+
+      const createTaskButton = screen.getByRole('button', { name: /create task/i });
       userEvent.click(createTaskButton);
-    })
+
+      const tasksTitles = screen.getAllByTestId('tasksTitles');
+      const tasksDates = screen.getAllByTestId('tasksDates');
+      const tasksTimestamps = screen.getAllByTestId('tasksTimestamps');
+      const tasksCreationDate = screen.getAllByTestId('tasksCreationDate');
+      const tasksDetails = screen.getAllByTestId('tasksDetails');
+      const tasksStatus = screen.getAllByTestId('tasksStatus');
+      
+      expect(tasksTitles).toBeInTheDocument();
+      expect(tasksDates).toBeInTheDocument();
+      expect(tasksTimestamps).toBeInTheDocument();
+      expect(tasksCreationDate).toBeInTheDocument();
+      expect(tasksDetails).toBeInTheDocument();
+      expect(tasksStatus).toBeInTheDocument();
+      
+    });
   });
 })
