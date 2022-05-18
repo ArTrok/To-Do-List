ecf0d037b409ebc6dba8005a6509ebc74c64b038
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 <artrok@github.com>
2022-05-18 15:47:00 -0300

    🚚 file: moved tests into src folder

--- frontend/src/tests/RenderWithRouter.js
+++ frontend/src/tests/RenderWithRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
-import App from '../src/App';
+import App from '../App';
 
 const renderWithRouter = () => {
   const history = createMemoryHistory();
