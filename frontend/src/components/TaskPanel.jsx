d92487fbee37233a5843693fc89a97321703d132
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 <artrok@github.com>
2022-05-18 23:43:32 -0300

    🚚 file: rename folder

--- frontend/src/components/TaskPanel.jsx
+++ frontend/src/components/TaskPanel.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const TaskPanel = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const test = async () => {
+      await axios.get('https://backend-portfolio-arthur.herokuapp.com/messager').then(res => {
+      if (res.data) {
+        setTasks(res.data);
+      }
+    })
+      .catch(err => console.log(err));
+    }
+
+    test();
+  }, []);
+
   return (
-    <div>TaskPanel</div>
+    <div>
+      {tasks.length < 1 && "No tasks"}
+    </div>
   )
 }
 
