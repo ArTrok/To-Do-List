25500c5c0797dd098564d66914e33e80edbb14dc
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 <artrok@github.com>
2022-05-19 22:44:13 -0300

    heroku deploy backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.DB_PORT;
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
